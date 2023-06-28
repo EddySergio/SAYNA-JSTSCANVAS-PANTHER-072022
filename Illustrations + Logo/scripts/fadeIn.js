ff3c4405fc64d292eee6e59dc5418e71128c3d0e
Sergio <zafindrambohoeddy@gmail.com>
2023-06-28 23:16:33 +0300

    ajout des elements de base avec un animation en js

---
+++ Illustrations + Logo/scripts/fadeIn.js
@@ -0,0 +1,15 @@
+const fadeInElement = document.querySelectorAll(".fadeInStart")
+
+
+function handleScroll() {
+  
+    fadeInElement.forEach((element) => {
+      const elementTop = element.getBoundingClientRect().top
+      const windowHeight = window.innerHeight
+  
+      if (elementTop < windowHeight) {
+        element.classList.add('fade-in')
+        
+      }
+    })
+}
