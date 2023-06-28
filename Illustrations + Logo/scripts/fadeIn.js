50d0a6fa15350c07bbef1fbe9c8cc0055bfa1c64
Sergio <zafindrambohoeddy@gmail.com>
2023-06-29 00:03:02 +0300

    mis a jour de code

--- Illustrations + Logo/scripts/fadeIn.js
+++ Illustrations + Logo/scripts/fadeIn.js
@@ -1,15 +1,16 @@
-const fadeInElement = document.querySelectorAll(".fadeInStart")
 
+const fadeInElement = document.querySelectorAll(".fadeInStart");
 
 function handleScroll() {
   
     fadeInElement.forEach((element) => {
-      const elementTop = element.getBoundingClientRect().top
-      const windowHeight = window.innerHeight
+      const elementTop = element.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
   
       if (elementTop < windowHeight) {
-        element.classList.add('fade-in')
+        element.classList.add('fadeIn');
         
-      }
+      };
     })
 }
+window.addEventListener('scroll', handleScroll);
